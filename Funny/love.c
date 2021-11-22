ee085691ea33f448f19e812f75d905ba769dc149
Tubbti <itstude@hotmail.com>
2021-11-22 19:22:48 +0800

    Update love

---
+++ Funny/love.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float x,y,a;
+    for(y=1.5f;y>-1.5f;y-=0.1f)
+    {
+        for(x=-1.5f;x<1.5f;x+=.05f)
+        {
+            a=x*x+y*y-1;
+            putchar(a*a*a-x*x*y*y*y<0.0f?'x':' ');
+        }
+        sleep(1);
+        putchar('\n');
+    }
+    getchar();
+    return 0 ;
+}
